a2896f313832eb0e7f42a16d75dbf20e295233b7
Valeria Ferreira Nocua <78361578+valeferreiraan@users.noreply.github.com>
2023-11-11 10:58:53 -0500

    index sin footer
    no se por que no quiere funcionaaar

--- ReactApp/glucotrack/src/pages/index/IndexApp.js
+++ ReactApp/glucotrack/src/pages/index/IndexApp.js
@@ -2,6 +2,12 @@ import Header from '../../components/header/Header';
 import Nav from '../../components/navbar/Nav';
 import Footer from '../../components/footer/Footer';
 import './IndexApp.css';
+import CardInfo from '../../components/cardInfo/cardInfo';
+import info1 from './img/1.png';
+import info2 from './img/2.png';
+import info3 from './img/3.png';
+import info4 from './img/reminders.png';
+import info5 from './img/medicalattention.png';
 
 
 
@@ -10,7 +16,57 @@ function IndexApp(){
         <div>
             <Header />
             <Nav />
-          
+            <div className="content">
+                <h1>¿Qué puedes hacer en GlucoTrack?</h1>
+                <div className="services">
+                    <CardInfo 
+                        image={info1}
+                        title = 'Agrega tus medidas'
+                        text1= ' ¡Al momento que lo hagas! Registra tus medidas para tener control de tus niveles de azúcar.'
+                        popover='Puedes ponerla al momento de medirte, o después y poner la hora.'
+                        backgroundColor='azulClaro1'
+                    />
+
+                    <CardInfo 
+                        image={info2}
+                        title = 'Lleva control'
+                        text1= ' Todas tus medidas quedarán guardadas para que puedas ver cómo se están comportando tus niveles de glucosa.'
+                        popover='Gráficas interactivas, por día, semana, o mes. '
+                        backgroundColor='azulprimario'
+                    />
+
+                    <CardInfo 
+                        image={info3}
+                        title = 'Aliméntate bien'
+                        text1= ' Podrás llevar control de las comidas que consumes, así, recordarás tus comidas y podrás relacionarlo con tus niveles de azúcar.'
+                        popover='Las comidas pueden influir más de lo que crees en tus niveles de glucosa. '
+                        backgroundColor='azulClaro1'
+                    />
+
+                    <CardInfo 
+                        image={info4}
+                        title = 'Alertas y recordatorios'
+                        text1= ' ¿Se te olvida aplicarte la insulina? Activa los recordatorios de insulinas y medicamentos para que no te vuelva a pasar.'
+                        popover='Es tú control, tú eres quien decide las horas de los recordatorios. '
+                        backgroundColor='azulprimario'
+                    />
+
+                    <CardInfo 
+                        image={info5}
+                        title = 'Atención médica'
+                        text1= 'Todo más fácil, manda directamente a tu médico/a tus niveles de azúcar para agilizar las consultas.'
+                        popover='En casos de emergencia, cuando el azúcar está muy alta o muy baja, nos podemos poner en contacto con tu médico/a. '
+                        backgroundColor='azulClaro1'
+                    />
+
+
+
+
+
+                </div>
+            </div>
+
+            
             
 
         </div>

852967371a9c28e047813ae2c374c7236c613a40
Valeria Ferreira Nocua <78361578+valeferreiraan@users.noreply.github.com>
2023-11-13 14:29:33 -0500

    sign up hecho

--- ReactApp/glucotrack/src/App.js
+++ ReactApp/glucotrack/src/App.js
@@ -5,11 +5,13 @@ import './App.css';
 import IndexApp from './pages/index/IndexApp';
 
 import Login from './pages/login/Login';
+import SignUp from './pages/login/Signup';
 
 function App() {
   return (
     <div className="container-fluid overflow-hidden">
-      <Login />
+      
+      <SignUp  />
     </div>
   );
 }
